bb923726dc4a81fe99e6cfd746a0ae49df818eb6
alexiusndoro <57504906+alexiusndoro@users.noreply.github.com>
2020-03-16 21:11:07 +0000

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,34 @@
+#loops 
+
+#range funtion
+
+#range 3 all numbers from 0-2
+'''print (range (3))
+
+#range (10,15) # all numbers in range 10-14
+
+for i in range (0,5):
+    print ('l')
+    
+player =  ('Benzema', 'Crouch', 'Defoe', 'Harewood')
+
+for i in player:
+    print (i)
+
+#while loop is only used while a condition is being met '''
+    
+lis =  range(0,11)
+
+
+i = 0
+
+
+while i in lis <= 5:
+    print(lis[i])
+
+    
+
+    
+
+
+# While Loop Example
