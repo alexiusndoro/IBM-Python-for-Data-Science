7f60f68e600f9db28746f31501bd799405f7460a
alexiusndoro <57504906+alexiusndoro@users.noreply.github.com>
2020-03-16 21:10:28 +0000

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,19 @@
+#making function:
+
+def summ(a):
+   b = a/6
+   return b
+    
+summ(8)
+
+#funtion can have multiple parameters 
+
+def Mult(a,b):
+    c=a*b
+    return c
+
+# can use 'pass' for funtion with empty body'
+def empty():
+    pass
+
+print (empty()) #returns none
